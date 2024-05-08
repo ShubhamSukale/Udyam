a9f52c340f97f6d30201543166a01033fedadbd7
shubh <shubh@HP-SHUBHAM>
2024-05-08 22:13:22 +0530

    Initial Commit

---
+++ app/src/test/java/com/udyam/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udyam.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
